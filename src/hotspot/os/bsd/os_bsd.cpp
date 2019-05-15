d6e36ec8abf15d6f990353cefffa83a758f3cba4
Johan Vos <johan.vos@gluonhq.com>
2019-05-15 20:33:25 +0200

    Make some functionality conditional on not being for IPHONE

--- src/hotspot/os/bsd/os_bsd.cpp
+++ src/hotspot/os/bsd/os_bsd.cpp
@@ -3679,6 +3679,7 @@ void os::pause() {
   }
 }
 
+#if ! TARGET_OS_IPHONE
 // Darwin has no "environ" in a dynamic library.
 #ifdef __APPLE__
   #include <crt_externs.h>
@@ -3752,6 +3753,10 @@ int os::fork_and_exec(char* cmd, bool use_vfork_if_available) {
     }
   }
 }
+#else // TARGET_OS_IPHONE
+int os::fork_and_exec(char* cmd, bool use_vfork_if_available) { return -1; }
+#endif // TARGET_OS_IPHONE
+
 
 // Get the default path to the core file
 // Returns the length of the string
